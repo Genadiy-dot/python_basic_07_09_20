96e710890728b600eba79999da9368aab99f7a59
Genadiy-dot <gem1@mail.ru>
2020-09-21 15:46:17 +0400

    Выполнено упражнение 3 с комментариями

--- task3.3.py
+++ task3.3.py
@@ -3,18 +3,18 @@
  которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов
 '''
 def my_func(arg_1, arg_2, arg_3):
-    sum = arg_1 + arg_2 + arg_3
-    if arg_1 < arg_2 and arg_1 < arg_3:
-        min = arg_1
+    summ = arg_1 + arg_2 + arg_3    # вычисляем сумму трёх аргументов
+    if arg_1 < arg_2 and arg_1 < arg_3:   # сумма двух наибольших=сумма-минимум
+        minn = arg_1
 
     elif arg_2 < arg_1 and arg_2 < arg_3:
-        min = arg_2
+        minn = arg_2
 
     else:
-        min = arg_3
+        minn = arg_3
 
-    sum_1 = sum - min
-    return(sum_1)
+    summ_1 = summ - minn
+    return(summ_1)
 arg_1 = float(input("Введите первый аргумент: "))
 arg_2 = float(input("Введите второй аргумент: "))
 arg_3 = float(input("Введите третий аргумент: "))
